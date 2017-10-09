1217c434d741f10f4333fffaab7a0aaa63966a95
thjiang <browserify@qq.com>
2017-10-09 21:42:50 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,19 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 var less = require('gulp-less');
+var cssmin = require('gulp-minify-css');
 var path = require('path');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('less', function() {
-	gulp.src('index.less')
+	gulp.src('styles/*.less')
         .pipe(less())
+		.pipe(cssmin())
+		.pipe(concat('index.css'))
 		.pipe(autoprefixer({
             browsers: ['last 5 versions', 'Android >= 4.0']
         }))
-        .pipe(gulp.dest(''));
+        .pipe(gulp.dest('styles'));
 });
 
 gulp.task('watch', function () {
